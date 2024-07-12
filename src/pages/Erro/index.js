144ad85578b3bee80a113cc5d37189723368fd4c
LucasRocha <lucaspdsr@gmail.com>
2024-07-12 14:14:22 -0300

    Adiciona estilização para a página de erro e ajustes de CSS para a página HOME e componentes Header e Footer. Implementa a funcionalidade de carregamento de filmes na página HOME com tratamento de erros.

--- src/pages/Erro/index.js
+++ src/pages/Erro/index.js
@@ -1,7 +1,11 @@
+import "./erro.css";
+
 function Erro() {
     return (
-        <div>
+        <div className="erro-container">
             <h1>404 - Página Não Encontrada</h1>
+            
+            <a href="/" className="voltar-home">Voltar para a Home</a>
         </div>
     );
 }
