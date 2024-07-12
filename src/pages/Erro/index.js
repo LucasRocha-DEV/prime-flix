d80278065f368bcb55592e8b6d2062b850f83492
LucasRocha <lucaspdsr@gmail.com>
2024-07-12 10:41:54 -0300

    - Criação das páginas Home, Filme e Erro com estruturas básicas. - Configuração das rotas no arquivo routes.js para navegação entre as páginas. - Adição de estilos globais em index.css para normalização e definição de estilos base. - Configuração do projeto React com ReactDOM.createRoot.

---
+++ src/pages/Erro/index.js
@@ -0,0 +1,9 @@
+function Erro() {
+    return (
+        <div>
+            <h1>404 - Página Não Encontrada</h1>
+        </div>
+    );
+}
+
+export default Erro;
---
+++ src/routes.js
@@ -0,0 +1,23 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import Home from './pages/HOME';
+import Filme from './pages/Filme';
+import Erro from './pages/Erro'; // Importe o componente Erro
+
+import Header from './components/Header';
+
+function RoutesApp() {
+    return (
+        <BrowserRouter>
+        <Header/>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/filme/:id" element={<Filme />} />
+                
+                <Route path="*" element={<Erro />} /> {/* Adicione esta linha */}
+            </Routes>
+        </BrowserRouter>
+    );
+}
+
+export default RoutesApp;
